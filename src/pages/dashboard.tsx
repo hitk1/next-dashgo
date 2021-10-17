2600ae5ee8fc90ad714ba90682da2da33cd23c0b
hitk1 <luispaulo.degini@gmail.com>
2021-10-17 17:19:36 -0300

    feat: Componentizacao e estilizacao da Sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,14 @@
+import { Flex } from '@chakra-ui/react'
+import { Sidebar } from '../components/Sidebar'
 import { Header } from '../components/Header'
 
 export default function Dashboard() {
     return (
-        <>
-            <Header></Header>
-        </>
+        <Flex direction="column" h="100vh">
+            <Header />
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+            </Flex>
+        </Flex>
     )
 }
