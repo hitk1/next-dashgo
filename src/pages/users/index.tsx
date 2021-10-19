960fe13de388b1256cd0245da7e49a399d971d7b
hitk1 <luispaulo.degini@gmail.com>
2021-10-18 22:29:34 -0300

    feat: Tela de listagem de usuários

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,99 @@
+import {
+    Box,
+    Flex,
+    Heading,
+    Button,
+    Icon,
+    Table,
+    Thead,
+    Tr,
+    Th,
+    Td,
+    Tbody,
+    Checkbox,
+    Text,
+} from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Header } from '../../components/Header'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function UserList() {
+    return (
+        <Box>
+            <Header />
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box
+                    flex="1"
+                    borderRadius={8}
+                    bg="gray.800"
+                    p="8"
+                >
+                    <Flex
+                        mb="8"
+                        align="center"
+                        justify="space-between"
+                    >
+                        <Heading
+                            size="lg"
+                            fontWeight="normal"
+                        >
+                            Usuários
+                        </Heading>
+                        <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="pink"
+                            leftIcon={<Icon as={RiAddLine} fontSize={20} />}
+                        >
+                            Criar novo
+                        </Button>
+                    </Flex>
+
+                    <Table>
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de Cadastro</Th>
+                                <Th w="1"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Luis Paulo</Text>
+                                        <Text fontSize="sm" color="gray.300">luispaulo.degini@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>
+                                    17 de outubro, 21
+                                </Td>
+                                <Td>
+                                    <Button
+                                        as="a"
+                                        size="sm"
+                                        fontSize="sm"
+                                        colorScheme="purple"
+                                        leftIcon={<Icon as={RiPencilLine} fontSize={16} />}
+                                    >
+                                        Editar
+                                    </Button>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </Box>
+
+            </Flex>
+        </Box>
+    )
+}
